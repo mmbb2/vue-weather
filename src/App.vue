f0fcde85982011a2bc26a6951af7f2078b24cc74
Maxym Tsymbalystyi <bakumax125@gmail.com>
2021-11-15 19:14:56 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <WeatherInfoCard :request="`api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f326b5463f56551f2590394732cd10d7`"/>
+  </div>
+</template>
+
+<script>
+import WeatherInfoCard from './components/WeatherInfoCard.vue'
+
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios)
+export default {
+  name: 'App',
+  data: function(){
+    return{
+      latitude: null,
+      longitude: null
+    }
+  },
+  components: {
+    WeatherInfoCard
+  },
+  mounted: function(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(({coords})=>{
+        this.latitude = coords.latitude
+        this.longitude = coords.longitude
+        console.log( this.latitude,  this.longitude)
+      })
+    
+  } else {
+    console.log('wefef')
+  }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/WeatherInfoCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="WeatherInfoCard">
+  
+  </div>
+</template>
+
+<script>
+
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios)
+export default {
+  name: 'WeatherInfoCard',
+  props: {
+    request: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted: function(){
+    console.log(this.request)
+   Vue.axios.get(this.request).then(([data])=>console.log(data))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
